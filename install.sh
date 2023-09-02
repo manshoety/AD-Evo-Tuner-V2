549bff4887fe708d7333400d0584983aa81f535b
artmamedov <artmame@gmail.com>
2023-09-02 03:19:03 -0400

    .sh for install and adt-user

--- install.sh
+++ install.sh
@@ -1,11 +1,43 @@
 #!/bin/bash
 
+# Locate the conda binary
+CONDA_BIN=$(which conda)
+
+# If conda is not found, exit
+if [ -z "$CONDA_BIN" ]; then
+    echo "Conda is not found in PATH."
+    exit 1
+fi
+
+# Infer the possible location of conda.sh based on the location of the conda binary
+CONDA_SH=$(dirname "$CONDA_BIN")/../etc/profile.d/conda.sh
+
+# Source conda.sh if it exists
+if [ -f "$CONDA_SH" ]; then
+    source "$CONDA_SH"
+else
+    echo "Could not find conda.sh script."
+    exit 2
+fi
+
 # activate the conda environment
-conda activate adt
+conda activate ADT
+
+# Check if activation was successful
+if [ $? -ne 0 ]; then
+    echo "Error activating conda environment"
+    exit 1
+fi
 
 # Download runwayml/StableDiffusion
 echo "Downloading runwayml/StableDiffusion..."
 python EZ_Facehugger.py
+
+# Check python script status
+if [ $? -ne 0 ]; then
+    echo "Error running EZ_Facehugger.py"
+    exit 2
+fi
 
 # Download the motion modules
 echo "Downloading motion modules..."
